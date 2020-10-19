75705edfa00e9ab5447d1038cb1ad9781009075d
Felix Jenkel (FG-432) <12564929+felixjenkel@users.noreply.github.com>
2020-10-19 17:32:26 +0200

    erased all dependecies to users api

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,6 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
 (async () => {
   await console.log( `server running ${config.url}` );
   await sequelize.addModels(V0_FEED_MODELS);
-  await sequelize.addModels(V0_USER_MODELS);
   await sequelize.sync();
 
   const app = express();
