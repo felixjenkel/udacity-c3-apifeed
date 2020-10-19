d35817aa4f53a7a8594df4d12bd70a295cb899db
Felix Jenkel (FG-432) <12564929+felixjenkel@users.noreply.github.com>
2020-10-19 14:49:13 +0200

    added travis-ci-support

--- src/aws.ts
+++ src/aws.ts
@@ -3,8 +3,9 @@ import {config} from './config/config';
 
 
 // Configure AWS
-const credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+const credentials = new AWS.SharedIniFileCredentials({profile: 'udacity_felix'});
 AWS.config.credentials = credentials;
+AWS.config.logger = console;
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
@@ -26,6 +27,7 @@ export function getGetSignedUrl( key: string ): string {
 // Generates an AWS signed URL for uploading objects
 export function getPutSignedUrl( key: string ): string {
   const signedUrlExpireSeconds = 60 * 5;
+  console.log(config.aws_media_bucket);
 
   return s3.getSignedUrl('putObject', {
     Bucket: config.aws_media_bucket,
--- src/server.ts
+++ src/server.ts
@@ -10,12 +10,15 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
 
 
 (async () => {
+  await console.log( `server running ${config.url}` );
   await sequelize.addModels(V0_FEED_MODELS);
   await sequelize.addModels(V0_USER_MODELS);
   await sequelize.sync();
 
   const app = express();
   const port = process.env.PORT || 8080;
+
+  console.log(port);
 
   app.use(bodyParser.json());
 
@@ -26,7 +29,7 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
       'X-Access-Token', 'Authorization',
     ],
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: config.url,
+    origin: "*",
   }));
 
   app.use('/api/v0/', IndexRouter);
